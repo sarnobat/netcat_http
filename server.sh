b8dd2adc0cb560442ce02e50fa909957aef004ca
sarnobat <ss401533@gmail.com>
2014-06-05 19:29:10 -0700

    Initial commit

--- server.sh
+++ server.sh
@@ -10,8 +10,8 @@ Content-Type: text/html\n\n' \
 $(cat ~/trash/test.eml) \
 	|  nc -l -p 8089 \
 	| grep "GET"
-} | grep "HTTP" \
-| perl -pe 's{.*param1=(.*).title=(.*).HTTP.1.1}{"$2","$1"\n$1\n}g' \
-| perl -pe 's/%([0-9a-f]{2})/sprintf("%s", pack("H2",$1))/eig' \
-| tee -a ~/work.txt; 
+}  	| grep "HTTP" \
+	| perl -pe 's{.*param1=(.*).title=(.*).HTTP.1.1}{"$2","$1"\n$1\n}g' \
+	| perl -pe 's/%([0-9a-f]{2})/sprintf("%s", pack("H2",$1))/eig' \
+	| tee -a ~/work.txt; 
 done;
