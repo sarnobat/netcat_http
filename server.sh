e592da7598832bb1203387041ecdcc10df3be653
sarnobat <ss401533@gmail.com>
2014-06-05 19:29:10 -0700

    Initial commit

--- server.sh
+++ server.sh
@@ -4,8 +4,11 @@ while true
 do
 {   
 	# For openbsd, add -p to nc
-	echo 'HTTP/1.1 200 OK\nDate: Fri, 30 May 2014 03:01:37 GMT\nAccess-Control-Allow-Origin: *\nServer: Apache/2.2.22 (Ubuntu)\nLast-Modified: Thu, 29 May 2014 03:26:28 GMT\nETag: "29ffda-3fb4-4fa817e984137"\nAccept-Ranges: bytes\nVary: Accept-Encoding\nContent-Type: text/html\n\n'$(cat ~/trash/test.eml) \
-	|  nc -l -p 8089   | grep "GET"
+	 echo 'HTTP/1.1 200 OK
+Date: Fri, 30 May 2014 03:01:37 GMT
+Access-Control-Allow-Origin: *
+Server: Apache/2.2.22 (Ubuntu)\nLast-Modified: Thu, 29 May 2014 03:26:28 GMT\nETag: "29ffda-3fb4-4fa817e984137"\nAccept-Ranges: bytes\nVary: Accept-Encoding\nContent-Type: text/html\n\n'$(cat ~/trash/test.eml) \
+|  nc -l -p 8089   | grep "GET"
 } | grep "HTTP" \
 | perl -pe 's{.*param1=(.*).title=(.*).HTTP.1.1}{"$2","$1"\n$1\n}g' \
 | perl -pe 's/%([0-9a-f]{2})/sprintf("%s", pack("H2",$1))/eig' \
